cf0390550ef8d1c5cc1e9a5270ca4b76f715c2e1
BJ Hansen <bjhansen@benjaminhansen.net>
2025-04-28 20:38:38 -0500

    initial

---
+++ src/Traits/Draftable.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace BenjaminHansen\FilamentDraftable\Traits;
+
+use Filament\Actions\Action;
+use Filament\Notifications\Notification;
+use Illuminate\Support\Facades\Cookie;
+
+trait Draftable
+{
+    public ?array $excludedFields = [];
+
+    public function saveDraft(): Action
+    {
+        // the key will be the slug of the current URL
+        $storageKey = str_slug($this->getUrl());
+        $data = $this->data;
+        $data_to_store = array_diff_key($data, array_flip($this->excludedFields));
+        $state = json_encode($data_to_store);
+
+        return Action::make('draftableSave')
+            ->label('Save Draft')
+            ->icon('heroicon-o-check')
+            ->action(function () use ($storageKey, $state) {
+                // store in a cookie
+                Cookie::queue($storageKey, $state, 9999999);
+
+                // Send a notification
+                Notification::make()
+                    ->title('Draft Saved')
+                    ->success()
+                    ->send();
+            });
+    }
+
+    public function loadDraft(): Action
+    {
+        // the key will be the slug of the current URL
+        $storageKey = str_slug($this->getUrl());
+
+        return Action::make('draftableLoad')
+            ->label('Load Draft')
+            ->icon('heroicon-o-arrow-up-tray')
+            ->action(function () use ($storageKey) {
+                // get the draft data from the cookie
+                $data = Cookie::get($storageKey);
+
+                if ($data) {
+                    $parsed = json_decode($data, true);
+                    foreach ($parsed as $key => $value) {
+                        if (array_key_exists($key, $this->data)) {
+                            $this->data[$key] = $value;
+                        }
+                    }
+
+                    // Send a notification
+                    Notification::make()
+                        ->title('Draft Loaded')
+                        ->success()
+                        ->send();
+                }
+            })
+            ->disabled(function () use ($storageKey) {
+                return ! Cookie::has($storageKey);
+            });
+    }
+}
